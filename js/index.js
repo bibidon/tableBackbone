cf2fa32a0a2c1247d9c52ae36ee635317e45f616
bibidon <debibidon@gmail.com>
2014-10-30 23:37:52 +0300

    rabota doma

--- js/index.js
+++ js/index.js
@@ -1,35 +1,23 @@
 ﻿$(function () {
-    var TableModel = Backbone.Model.extend({});
 
-    var tableModel = new TableModel();
+    var allObject = [
+    { name: "bread", description: "white", price: 5, quantity: 10, id: "bottom" },
+    { name: "butter", description: "soft", price: 2, quantity: 20, id: "middle" },
+    { name: "sausage", description: "cooked", price: 15, quantity: 5, id: "top" }];
+
+    var TableModel = Backbone.Model.extend({
+        defaults: {
+            name: "",
+            description: "",
+            price: "",
+            quantity: "",
+            id: ""
+        }
+    });
 
     var TableCollectionModel = Backbone.Collection.extend({
         model: TableModel
     });
-
-    var tableCollectionModel = new TableCollectionModel([
-        {
-            name: "bread",
-            description: "white",
-            price: 5,
-            quantity: 10,
-            id: "bottom"
-        },
-        {
-            name: "butter",
-            description: "soft",
-            price: 2,
-            quantity: 20,
-            id: "middle"
-        },
-        {
-            name: "sausage",
-            description: "cooked",
-            price: 15,
-            quantity: 5,
-            id: "top"
-        }
-    ]);
 
     var TableView = Backbone.View.extend({
         el: $(".exampleTable"),
@@ -45,40 +33,59 @@
 
     var TrView = Backbone.View.extend({
         el: $("#newTr"),
+
         template: _.template($("#addTr").html()),
-        initialize: function () { this.render(); },
+
+        initialize: function () {
+            this.render();
+            this.collection.on("add", this.render, this);
+        },
+
         renderTr: function (item) {
             this.model = item;
             this.$el.before(this.template(this.model.toJSON()));
             return this;
         },
+
         render: function () {
+            this.collection = new TableCollectionModel(allObject);
             _.each(this.collection.models, function (item) {
                 this.renderTr(item);
             }, this);
-        }
-    });
-
-    var trView = new TrView({ collection: tableCollectionModel });
-
-    var TrAddView = Backbone.View.extend({
-        el: $("tr:last-child > td"),
-        buttonPlus: $("tr:last-child > td > button.btn-plus"),
-        btnAddOk: $("tr:last-child > td > button.btn-addok"),
-        btnAddCansel: $("tr:last-child > td > button.btn-addcansel"),
-        events: {
-            "click buttonPlus": "textareaAndButton"
         },
+
+        events: {
+            "click .btn-plus": "textareaAndButton",
+            "click .btn-addok": "saveNewTr",
+        },
+
         textareaAndButton: function () {
-            this.buttonPlus.hide();
-            btnAddOk.visible();
-            btnAddCansel.visible();
-            _.each(this.$el, function (td) {
+            $("tr:last-child > td > button.btn-plus").addClass("btn-hidden");
+            $("tr:last-child > td > button.btn-addok").addClass("btn-visible");
+            $("tr:last-child > td > button.btn-addcansel").addClass("btn-visible");
+            _.each($("tr:last-child > td"), function (td) {
                 if (td.classList.length != 0) return;
                 td.innerHTML = "<textarea cols='10' rows='1'></textarea>";
             });
+        },
+
+        saveNewTr: function () {
+            var newModel = {
+                "name": "",
+                "description": "",
+                "price": "",
+                "quantity": ""
+            };
+            var lastTrTd = $("tr:last-child > td");
+            _.each(lastTrTd, function (td) {
+                if (td.classList.length != 0) return;
+                newModel[td.getAttribute("name")] = $(td.children).val();
+            });
+
+            allObject.push(newModel);
+            this.collection.add(new TableModel(newModel));
         }
     });
 
-    var trAddView = new TrAddView();
+    var trView = new TrView();
 })
