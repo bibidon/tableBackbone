2768ef7c934fd9bd1ae3d4f984fe946df936d9e0
bibidon <debibidon@gmail.com>
2014-11-10 23:28:38 +0300

    doma 10.11.2014

---
+++ js/Models/ModelsAndCollections.js
@@ -0,0 +1,23 @@
+﻿define(function () {
+
+    function doModelAndCollection() {
+
+        //функция-конструктор модели
+        this.TableModel = Backbone.Model.extend({
+            defaults: {
+                name: "",
+                description: "",
+                price: "",
+                quantity: "",
+                id: ""
+            }
+        });
+
+        //функция-конструктор колекции 
+        this.TableCollectionModel = Backbone.Collection.extend({
+            model: TableModel
+        });
+    }
+
+    return { doModelAndCollection: doModelAndCollection };
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿require(["Models/data"], function (data) {
+
+    var allModels = [new data({ name: "bread", description: "white", price: 5, quantity: 10, id: "bottom" }),
+        new data({ name: "butter", description: "soft", price: 2, quantity: 20, id: "middle" }),
+        new data({ name: "sausage", description: "cooked", price: 15, quantity: 5, id: "top" })];
+
+});
