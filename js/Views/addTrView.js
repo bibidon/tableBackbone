130de5556bc157679cd2a2c37cb7f5a8e575d33b
bibidon <debibidon@gmail.com>
2014-11-17 16:16:12 +0300

    on work 17.11.2014

--- js/Views/addTrView.js
+++ js/Views/addTrView.js
@@ -7,6 +7,7 @@
         initialize: function () {
             this.collection = collection;
             this.render();
+            this.collection.on("add", this.render, this);
         },
 
         renderTr: function (item) {
@@ -21,7 +22,62 @@
             _.each(this.collection.models, function (item) {
                 this.renderTr(item);
             }, this);
-        }
+        },
+
+
+        events: function () {
+            $(".btn-plus").click(this.textareaAndButton);
+            $(".btn-addok").click(this.saveNewTr);
+            $(".btn-addok").click(this.visibleAndHidde);
+        },
+
+        idFunction: function () {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                           .toString(16)
+                           .substring(1);
+            }
+            return function () {
+                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                       s4() + '-' + s4() + s4() + s4();
+            };
+        },
+
+        textareaAndButton: function () {
+            $("#newTr > td > button.btn-plus").addClass("btn-hidden");
+            $("#newTr > td > button.btn-addok").addClass("btn-visible");
+            $("#newTr > td > button.btn-addcansel").addClass("btn-visible");
+            _.each($("#newTr > td"), function (td) {
+                if (td.classList.length != 0) return;
+                td.innerHTML = "<textarea cols='10' rows='1'></textarea>";
+            });
+        },
+
+        saveNewTr: function () {
+            var id = this.idFunction();
+            var newModel = {
+                "name": "",
+                "description": "",
+                "price": "",
+                "quantity": "",
+                "id": id()
+            };
+            var lastTrTd = $("#newTr > td");
+            _.each(lastTrTd, function (td) {
+                if (td.classList.length != 0) return;
+                newModel[td.getAttribute("name")] = $(td.children).val();
+            });
+            allObject.push(newModel);
+            //this._addLocalStorage();
+            this.collection.add(new TableModel(newModel));
+        },
+
+        visibleAndHidde: function () {
+            $("textarea").remove();
+            $("#newTr > td > button.btn-plus").removeClass("btn-hidden");
+            $("#newTr > td > button.btn-addok").removeClass("btn-visible");
+            $("#newTr > td > button.btn-addcansel").removeClass("btn-visible");
+        },
     });
 
     return View;
